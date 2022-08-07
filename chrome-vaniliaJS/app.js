2d35a102a782c42a5e4f297fc8a39489342cedfd
seonb2n <626ksb@gmail.com>
2022-08-07 22:32:26 +0900

    toodle 로그인 화면 중 login-form 구현

--- chrome-vaniliaJS/app.js
+++ chrome-vaniliaJS/app.js
@@ -3,14 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USER_NAME_KEY = "user";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add("hidden");
-    console.log(username);
+    localStorage.setItem(USER_NAME_KEY, username);
+    paintGreeting(username);
+}
+
+function paintGreeting(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USER_NAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+    paintGreeting(savedUsername);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
