533aade56d48ab6ba05df2587dac8457991cc4d2
seonb2n <626ksb@gmail.com>
2022-08-15 17:08:33 +0900

    로그인 버튼 fetch 기능 구현

--- toodle-vanilaJS/login/js/app.js
+++ toodle-vanilaJS/login/js/app.js
@@ -1,8 +1,9 @@
-const emailInput = document.querySelector(".login_email_input")
-const passwordInput = document.querySelector(".login_password_input")
+const emailInput = document.querySelector(".login_email_input");
+const passwordInput = document.querySelector(".login_password_input");
 const emailInputFocusLine = document.querySelector(".login_email_focus_line");
 const passwordInputFocusLine = document.querySelector(".login_password_focus_line");
-const password_show_btn = document.querySelector(".login_password img")
+const passwordShowBtn = document.querySelector(".login_password img");
+const loginBtn = document.querySelector(".login_form_submit_button");
 const HIDDEN_CLASSNAME = "hidden";
 
 emailInput.onfocus = function (e) {
@@ -21,14 +22,42 @@ passwordInput.onblur = function (e) {
     passwordInputFocusLine.classList.add(HIDDEN_CLASSNAME);
 }
 
-function onShowPassword(event) {
+function onShowPassword() {
     if (passwordInput.getAttribute("type") === "password") {
         passwordInput.setAttribute("type", "text");
-        password_show_btn.src = "src/ic_task_look_on.png";
+        passwordShowBtn.src = "src/ic_task_look_on.png";
     } else {
         passwordInput.setAttribute("type", "password");
-        password_show_btn.src = "src/ic_task_look_off.png";
+        passwordShowBtn.src = "src/ic_task_look_off.png";
     }
 }
 
-password_show_btn.addEventListener("click", onShowPassword);
+function onLoginBtnClicked(e) {
+    e.preventDefault();
+    const userEmail = emailInput.value;
+    const userPassword = passwordInput.value;
+    fetch("http://192.168.35.157:8080/api/v1/users/login", {
+        method: "POST",
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        redirect: 'follow',
+        body: JSON.stringify({
+            userEmail: userEmail,
+            password: userPassword,
+        }),
+    }).then((response) => {
+        console.log(response);
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        const userName = data;
+    })
+        .catch(error => console.log(error));
+
+}
+
+passwordShowBtn.addEventListener("click", onShowPassword);
+loginBtn.addEventListener("click", onLoginBtnClicked);
